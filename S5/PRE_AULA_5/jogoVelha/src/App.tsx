8915f24f8a82a9f495837f80e66cae2232baa8f7
Elias Everson <96179285+SRELIAS@users.noreply.github.com>
2023-03-29 12:25:46 -0300

    V1.0.1 Adicionando jogo da velha pre aula 5

---
+++ S5/PRE_AULA_5/jogoVelha/src/App.tsx
@@ -0,0 +1,28 @@
+/* Curso de Engenharia de Software - UniEVANGÉLICA 
+Disciplina de Programação Web 
+Aluno: Elias Everson da Silva Soares– 211983
+DATA: 29/03/2023 */
+
+
+import React, { useState } from "react";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './assets/css/style.css';
+
+
+import TicTacToe from "./Componentes/TicTacToe";
+import Board from "./Componentes/Board";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Board></Board>
+      <TicTacToe></TicTacToe>
+    </div>
+  )
+}
+
+export default App
---
+++ S5/PRE_AULA_5/jogoVelha/src/main.tsx
@@ -0,0 +1,14 @@
+/* Curso de Engenharia de Software - UniEVANGÉLICA 
+Disciplina de Programação Web 
+Dev: Luiz Henrique Bispo 
+DATA: 26/03/2023 */
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
